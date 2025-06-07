fba0c79359ad8a59780df0016a212f8153a9e17e
867378 <ongvincentdavid@gmail.com>
2025-06-08 01:23:44 +0800

    Yes

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import App from "./App.vue";
 import router from "./router";
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:8000/api";
+axios.defaults.baseURL = "https://gcch-backend.onrender.com/api";
 axios.defaults.withCredentials = true;
 axios.defaults.withXSRFToken = true;
 
