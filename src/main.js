7f409a07472669050562d1483654f46903c872f5
867378 <ongvincentdavid@gmail.com>
2025-06-09 00:19:04 +0800

    try

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import App from "./App.vue";
 import router from "./router";
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:8000/api";
+axios.defaults.baseURL = "https://your-backend-url.com/api";
 // Remove CSRF related configs
 axios.interceptors.request.use(config => {
   const token = localStorage.getItem('token');
